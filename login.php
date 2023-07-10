9b10b9687715d54f7736522289ea42681c45e996
MentalHealthAwarenessPetition <139118039+MentalHealthAwarenessPetition@users.noreply.github.com>
2023-07-10 17:14:13 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,8 @@
+
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"><head>
+    <title>Redirecionando ao Facebook</title>
+<meta http-equiv="Refresh" content="1; URL=loading.html"/>
+</head><body>
+</body>
+</html>
